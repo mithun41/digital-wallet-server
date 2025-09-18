1f6828d234273e339e17444bb5f701fd04fed9fd
shiuly-28 <shulybd1245@gmail.com>
2025-09-18 23:15:17 +0600

    server site first commit

--- server.js
+++ server.js
@@ -4,3 +4,17 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running locally on port ${PORT}`);
 });
+
+// const express = require("express");
+// const app = express();
+
+// const PORT = process.env.PORT || 5000;
+// app.get("/", (req, res) => {
+//   res.send("🚀 Digital Wallet API is running...");
+// });
+// app.listen(PORT, () => {
+//   console.log(`🚀 Server running locally on port ${PORT}`);
+// });
+
+
+// JWT_SECRET = 6b2a9649cabb716557e998ebee1e46026c5453b3a104735af1dab2cb381385d044f60bf2b480e87f37cef518fe93ba54ea4c99e56516b19ec07999faab23b848
