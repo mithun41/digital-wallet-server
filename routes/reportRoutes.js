fc23f94f40c79057262d304356a413511475d99d
mithun8441 <mahmudulhasan3527@gmail.com>
2025-10-23 00:04:23 +0600

    updated loans api

--- routes/reportRoutes.js
+++ routes/reportRoutes.js
@@ -2,12 +2,10 @@ const express = require("express");
 const { protectByToken } = require("../middleware/authMiddleware");
 
 const { adminProtect } = require("../middleware/adminMiddleware");
-const { userReport } = require("../controllers/reportcontrollers");
-
+const { userReport } = require("../controllers/reportControllers");
 
 const router = express.Router();
 
-
-router.post("/", userReport )
+router.post("/", userReport);
 
 module.exports = router;
